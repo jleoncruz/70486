57e9521c1037903e51dba640232560739034408a
jleoncruz <Juan.Leon-Cruz@asmr.com>
2017-02-02 15:01:52 -0500

    Basic Tutorial
    Basic Tutorial Completed!

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -7,14 +7,28 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
+
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
+        //[Display(Name = "Release Date"), DataType(DataType.Date)]  //<-- One line coding
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+
+        [Range(1, 100)]
         [DataType(DataType.Currency)]
+        //[Range(1, 100), DataType(DataType.Currency)]  //<-- One line coding
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(5)]
         public string Rating { get; set; }
     }
 
