1b3db3270bba686eb544b0cae5b9c8a290dfd7a3
JuanPablo <JuanPablo@DESKTOP-QMM708D>
2017-02-05 22:42:42 -0500

    Updating Related Data: Adding an Edit Page for Instructors

---
+++ ContosoUniversity/Models/Instructor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Instructor
+    {
+        public int ID { get; set; }
+
+        //[Required]
+        //[Display(Name = "Last Name")]
+        //[StringLength(50)]
+        [Display(Name = "Last Name"), StringLength(50, MinimumLength = 1)]
+        public string LastName { get; set; }
+
+        //[Required]
+        //[Column("FirstName")]
+        //[Display(Name = "First Name")]
+        //[StringLength(50)]
+        [Column("FirstName"), Display(Name = "First Name"), StringLength(50, MinimumLength = 1)]
+        public string FirstMidName { get; set; }
+
+        //[DataType(DataType.Date)]
+        //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        //[Display(Name = "Hire Date")]
+        [DataType(DataType.Date), Display(Name = "Hire Date"), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime HireDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get { return LastName + ", " + FirstMidName; }
+        }
+
+        public virtual ICollection<Course> Courses { get; set; }
+        public virtual OfficeAssignment OfficeAssignment { get; set; }
+
+        //virtual == Lazy Loading....
+        //ICollection = Multiple Entities
+    }
+}
