30e3f7b116b0fd812ebe020730aa81539e4baf06
jleoncruz <Juan.Leon-Cruz@asmr.com>
2017-02-07 13:56:45 -0500

    Implementing Inheritance
    Implementing Inheritance Complete

--- ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -10,11 +10,12 @@ namespace ContosoUniversity.DAL
         //{
         //}
 
+        public DbSet<Person> People { get; set; }
         public DbSet<Student> Students { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Course> Courses { get; set; }
         public DbSet<Department> Departments { get; set; }
-        public DbSet<Instructor> Instructors { get; set; }
         public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
--- ContosoUniversity/Models/Instructor.cs
+++ ContosoUniversity/Models/Instructor.cs
@@ -1,38 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
-    public class Instructor
+    public class Instructor : Person
     {
-        public int ID { get; set; }
-
-        //[Required]
-        //[Display(Name = "Last Name")]
-        //[StringLength(50)]
-        [Display(Name = "Last Name"), StringLength(50, MinimumLength = 1)]
-        public string LastName { get; set; }
-
-        //[Required]
-        //[Column("FirstName")]
-        //[Display(Name = "First Name")]
-        //[StringLength(50)]
-        [Column("FirstName"), Display(Name = "First Name"), StringLength(50, MinimumLength = 1)]
-        public string FirstMidName { get; set; }
-
         //[DataType(DataType.Date)]
         //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         //[Display(Name = "Hire Date")]
         [DataType(DataType.Date), Display(Name = "Hire Date"), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime HireDate { get; set; }
-
-        [Display(Name = "Full Name")]
-        public string FullName
-        {
-            get { return LastName + ", " + FirstMidName; }
-        }
 
         public virtual ICollection<Course> Courses { get; set; }
         public virtual OfficeAssignment OfficeAssignment { get; set; }
