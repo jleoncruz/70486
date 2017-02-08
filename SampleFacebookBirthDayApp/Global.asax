663c17d0b5761992d40cfce1de1a269ddb3c4613
JuanPablo <JuanPablo@DESKTOP-QMM708D>
2017-02-07 22:25:54 -0500

    Azure App Service Plus Facebook and Google OAuth2

---
+++ SampleFacebookBirthDayApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleFacebookBirthDayApp.MvcApplication" Language="C#" %>
---
+++ SampleFacebookBirthDayApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleFacebookBirthDayApp.Startup))]
+namespace SampleFacebookBirthDayApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
