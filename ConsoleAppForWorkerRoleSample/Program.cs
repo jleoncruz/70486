71e84804e88b81cfe7c82c15cb84d80fd6f92890
jleoncruz <Juan.Leon-Cruz@asmr.com>
2017-02-13 20:22:41 -0500

    Azure Role Life Cycle
    Demo/Video/Tutorial

---
+++ ConsoleAppForWorkerRoleSample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppForWorkerRoleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var process = Process.Start(@"C:\Windows\SysWOW64\calc.exe");
+            process.WaitForExit();
+        }
+    }
+}
---
+++ ConsoleAppForWorkerRoleSample/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleAppForWorkerRoleSample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ASM Research")]
+[assembly: AssemblyProduct("ConsoleAppForWorkerRoleSample")]
+[assembly: AssemblyCopyright("Copyright © ASM Research 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("92d0718b-c768-4ae5-b4bc-fef26953b64c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
