69c03a57e4a01fc583fd1ac34896ea4126688a85
JuanPablo <JuanPablo@DESKTOP-QMM708D>
2017-02-21 23:13:13 -0500

    Pluralsight Tdd Module, completed!

---
+++ OdeToFood.Tests/Controllers/HomeControllerTestM.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OdeToFood.Controllers;
+using OdeToFood.Models;
+using OdeToFood.Tests.Fakes;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace OdeToFood.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTestM
+    {
+        [TestMethod]
+        public void Index()
+        {
+            //Arrange
+            var db = new FakeOdeToFoodDb();
+            db.AddSet(TestData.Restaurants);
+
+            HomeController controller = new HomeController(db);
+            controller.ControllerContext = new FakeControllerContext();
+
+            //Act
+            ViewResult result = controller.Index() as ViewResult;
+            IEnumerable<RestaurantListViewModel> model = result.Model as IEnumerable<RestaurantListViewModel>;
+
+            //Assert
+            Assert.AreEqual(10, model.Count());
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            //Act
+            ViewResult result = controller.About() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result.Model);
+        }
+
+        [TestMethod]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ OdeToFood.Tests/Features/RestaurantRater.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using OdeToFood.Models;
+using System.Linq;
+
+namespace OdeToFood.Tests.Features
+{
+    public class RestaurantRater
+    {
+        private Restaurant _restaurant;
+
+        public RestaurantRater(Restaurant restaurant)
+        {
+            this._restaurant = restaurant;
+        }
+
+        public RatingResult ComputeResult(IRatingAlgorithm algorithm, int numberOfReviewsToUse)
+        {
+            var filteredReviews = _restaurant.Reviews.Take(numberOfReviewsToUse);
+
+            //return algorithm.Compute(_restaurant.Reviews.ToList());
+            return algorithm.Compute(filteredReviews.ToList());
+        }
+    }
+}
